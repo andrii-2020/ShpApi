425771fb7a35d9f3e99fd2572928ce436dec3918
andrii-2020 <bebion58@gmail.com>
2025-02-08 10:44:15 +0100

    whitenoise

--- Mshop/settings.py
+++ Mshop/settings.py
@@ -72,6 +72,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     
 ]
 
@@ -144,7 +145,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 APPEND_SLASH = False
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "mystaticfiles")]
 MEDIA_URL = '/media/'
 
 # Default primary key field type
