ec7f879acb1ba66886d9d6d54b27428dcdf68386
andrii-2020 <bebion58@gmail.com>
2025-02-08 11:08:11 +0100

    media

--- Mshop/settings.py
+++ Mshop/settings.py
@@ -147,6 +147,7 @@ APPEND_SLASH = False
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "mystaticfiles")]
 MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 
