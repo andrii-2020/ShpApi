787d48b46cd4b8d6f9ace794c258f63bfafe6410
andrii-2020 <bebion58@gmail.com>
2025-02-17 14:47:49 +0100

    ...

--- Mshop/settings.py
+++ Mshop/settings.py
@@ -15,9 +15,9 @@ SECRET_KEY = env("SECRET_KEY")
 
 DEBUG = env("DJANGO_DEBUG")
 
-ALLOWED_HOSTS = ['https://shpapi-production.up.railway.app']
+ALLOWED_HOSTS = ['shpapi-production.up.railway.app', '127.0.0.1', 'localhost']
 
-CSRF_TRUSTED_ORIGINS = ['https://shpapi-production.up.railway.app']
+CSRF_TRUSTED_ORIGINS = ['shpapi-production.up.railway.app']
 
 CORS_ALLOW_ALL_ORIGINS = True
 
