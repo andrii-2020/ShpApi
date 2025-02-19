3daf7c2ca364b53106827846287cd625e0ef1923
andrii-2020 <bebion58@gmail.com>
2025-02-19 13:13:52 +0100

    ...

--- Mshop/settings.py
+++ Mshop/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = env("SECRET_KEY")
 
 DEBUG = env("DJANGO_DEBUG")
 
-ALLOWED_HOSTS = ['https://*.shopmakss.netlify.app', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['83.25.177.56', '127.0.0.1', 'localhost']
 
 CSRF_TRUSTED_ORIGINS = ['https://shpapi-production.up.railway.app']
 
