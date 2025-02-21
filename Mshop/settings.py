262cd484e98ff2941199263d890a62e358450218
andrii-2020 <bebion58@gmail.com>
2025-02-21 17:40:31 +0100

    update

--- Mshop/settings.py
+++ Mshop/settings.py
@@ -60,7 +60,7 @@ MIDDLEWARE = [
     
 ]
 
-ALLOWED_HOSTS = ['https://shopmakss.netlify.app']
+ALLOWED_HOSTS = ['*']
 
 CORS_ORIGIN_ALLOW_ALL = False
 #CORS_ALLOWED_ORIGINS = ['https://shopmakss.netlify.app']
